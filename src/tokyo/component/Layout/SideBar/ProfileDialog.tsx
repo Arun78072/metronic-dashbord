12611bfebe088df3d88e23bc5b4f4490d3526263
fdev1.delta4 <fdev1.delta4@gmail.com>
2022-08-23 19:05:19 +0530

    update 23-08

--- src/tokyo/component/Layout/SideBar/ProfileDialog.tsx
+++ src/tokyo/component/Layout/SideBar/ProfileDialog.tsx
@@ -1,17 +1,17 @@
 
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemText from '@mui/material/ListItemText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Dialog from '@mui/material/Dialog';
-import PersonIcon from '@mui/icons-material/Person';
-import AddIcon from '@mui/icons-material/Add';
-import Typography from '@mui/material/Typography';
-import { blue } from '@mui/material/colors';
+// import Avatar from '@mui/material/Avatar';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemAvatar from '@mui/material/ListItemAvatar';
+// import ListItemText from '@mui/material/ListItemText';
+// import DialogTitle from '@mui/material/DialogTitle';
+// import Dialog from '@mui/material/Dialog';
+// import PersonIcon from '@mui/icons-material/Person';
+// import AddIcon from '@mui/icons-material/Add';
+// import Typography from '@mui/material/Typography';
+// import { blue } from '@mui/material/colors';
 import styled, { useTheme } from 'styled-components';
 import { Link } from 'react-router-dom';
 import { rgba } from 'polished';
